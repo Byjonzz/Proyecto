c612fa7ad638a0483d7142e17dbe79ae7b12c368
Jonathan Alexis Alta Bravo <bravojonathan95@gmail.com>
2026-07-09 12:33:41 -0600

    Actualizacion '09/07/2026' sistema actualizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://10.144.86.55:1423/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 const api = axios.create({
